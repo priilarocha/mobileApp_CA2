c55bc915233712527b77899f4877433176ff4fda
priirocha <prii.rocha@hotmail.com>
2023-04-04 14:21:36 +0100

    Created the Recycler View.

--- app/src/main/java/com/example/ca2/Movie.kt
+++ app/src/main/java/com/example/ca2/Movie.kt
@@ -5,7 +5,7 @@ data class Movie (
     val image: String,
     val certification: String,
     val description: String,
-    var starring: Array<String>,
+    var starring: String,
     var running_time_mins: Int,
     var seats_remaining: Int,
     var seats_selected: Int = 0
--- app/src/main/java/com/example/ca2/MovieAdaptor.kt
+++ app/src/main/java/com/example/ca2/MovieAdaptor.kt
@@ -1,4 +1,34 @@
 package com.example.ca2
 
-class MovieAdaptor {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_movie.view.*
+
+class MovieAdaptor(
+    var movies: List<Movie>
+) : RecyclerView.Adapter<MovieAdaptor.MovieViewHolder>() {
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.itemView.apply {
+            movieTitleTextView.text = movies[position].name
+            movieStarNameTextView.text = movies[position].starring
+            movieTimeValueTextView.text = movies[position].running_time_mins.toString()
+            movieSeatsRemainingTextView.text = movies[position].seats_remaining.toString()
+
+        }
+    }
 }
