a980f1720c108c1fd8e91167c811549f386c916e
priirocha <prii.rocha@hotmail.com>
2023-04-04 14:41:43 +0100

    Add Moshi library with Retrofit to parse the JSON API

--- app/src/main/java/com/example/ca2/Movie.kt
+++ app/src/main/java/com/example/ca2/Movie.kt
@@ -1,12 +1,12 @@
 package com.example.ca2
 
 data class Movie (
-    val name: String,
+    val title: String,
     val image: String,
-    val certification: String,
-    val description: String,
-    var starring: String,
-    var running_time_mins: Int,
-    var seats_remaining: Int,
-    var seats_selected: Int = 0
+    val contentRating: String,
+    val plot: String,
+    var stars: String,
+    var runtimeStr: String,
+//    var seats_remaining: Int,
+//    var seats_selected: Int = 0
 )
